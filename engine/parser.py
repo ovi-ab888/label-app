1ba69efcd45443205c5e247dc680844ace967aae
ovi-ab888 <oviabdulohab@gmail.com>
2025-08-18 17:59:08 +0600

    Update parser.py

--- engine/parser.py
+++ engine/parser.py
@@ -1,1 +1,7 @@
+from io import BytesIO, StringIO
+from typing import List, Tuple, Union, IO
+...
+def load_csv(src: Union[str, BytesIO, StringIO, IO[str]]) -> Tuple[pd.DataFrame, List[str]]:
+    df = pd.read_csv(src)
+    ...
 
